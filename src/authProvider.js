408909c75b5f7b2084a16694145c969242a76b14
jantan <jantanzulfikar@gmail.com>
2019-07-25 15:52:57 +0700

    tst apakah 2

--- src/authProvider.js
+++ src/authProvider.js
@@ -11,7 +11,7 @@ export default (type, params) => {
         const request = new Request('https://monitoring-api.herokuapp.com/api/v1/user/loginAdmin', {
             method: 'OPTIONS',
             body: JSON.stringify({ username, password }),
-             headers: new Headers({'Authorization': 'ApiAuth api_key=DMA128256512AI'
+             headers: new Headers({'Content-Type': 'application/json;charset=utf-8','Authorization': 'ApiAuth api_key=DMA128256512AI'
              ,'Access-Control-Allow-Origin':'*' }),
         })
         return fetch(request)
